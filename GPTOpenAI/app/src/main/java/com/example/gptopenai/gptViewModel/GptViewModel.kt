d5274cb87ef410e2372cf733c87255e4912917c9
Bhavye <bhavyedevelopment2003@gmail.com>
2023-09-10 12:19:22 +0530

    created GPT OpenAI app

---
+++ GPTOpenAI/app/src/main/java/com/example/gptopenai/gptViewModel/GptViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.gptopenai.gptViewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.gptopenai.network.ChatService
+import com.google.gson.JsonParser
+import kotlinx.coroutines.launch
+
+class GptViewModel : ViewModel() {
+
+    private val jsonParser = JsonParser()
+
+    private val _listChats: MutableLiveData<MutableList<Chat>> = MutableLiveData(mutableListOf())
+    val listChats: LiveData<MutableList<Chat>> = _listChats
+
+    fun submitUserQuery(query: String) {
+
+        val list = _listChats.value
+
+        viewModelScope.launch {
+            try
+            {
+                val chatService = ChatService(query)
+                val response = chatService.getAnswer()
+
+                val answer =
+                    jsonParser.parse(response).asJsonObject.getAsJsonArray("choices")[0].asJsonObject.getAsJsonObject(
+                        "message"
+                    ).get("content").asString
+                list?.add(Chat(query, answer))
+                _listChats.postValue(list)
+            }
+            catch (_: java.lang.Exception){
+                list?.add(Chat(query, "Error : Please try again"))
+                _listChats.postValue(list)
+            }
+        }
+    }
+}
